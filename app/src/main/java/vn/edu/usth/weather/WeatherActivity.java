047efee8695ef3123c3a37c628401075ba1a4b13
Nguyễn Gia Phong <mcsinyx@disroot.org>
2020-12-11 10:00:40 +0700

    Start fresh with just logs

---
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,55 @@
+package vn.edu.usth.weather;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i("USTH Weather", "on create");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.i("USTH Weather", "on start");
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.i("USTH Weather", "on restart");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.i("USTH Weather", "on resume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i("USTH Weather", "on pause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i("USTH Weather", "on stop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i("USTH Weather", "on destroy");
+        super.onDestroy();
+    }
+}
