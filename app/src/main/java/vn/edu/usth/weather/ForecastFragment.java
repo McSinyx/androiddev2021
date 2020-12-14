384a51940401d9a8445648ada7cc1ad52523baa5
Nguyễn Gia Phong <mcsinyx@disroot.org>
2020-12-14 17:41:13 +0700

    Makes everything static

--- app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -12,22 +12,4 @@ import androidx.fragment.app.Fragment;
 
 public class ForecastFragment extends Fragment {
     public ForecastFragment() { super(R.layout.fragment_forecast); }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        LinearLayout linear = (LinearLayout) super.onCreateView(
-            inflater, container, savedInstanceState);
-        linear.setOrientation(LinearLayout.VERTICAL);
-
-        TextView text = new TextView(getContext());
-        text.setText("Thursday");
-        linear.addView(text);
-
-        ImageView image = new ImageView(getContext());
-        image.setImageResource(R.drawable.showers);
-        linear.addView(image);
-
-        return linear;
-    }
 }
--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -10,49 +10,4 @@ import vn.edu.usth.weather.ForecastFragment;
 
 public class WeatherActivity extends AppCompatActivity {
     public WeatherActivity() { super(R.layout.activity_weather); }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        Log.i("USTH Weather", "on create");
-        super.onCreate(savedInstanceState);
-        if (savedInstanceState == null)
-            getSupportFragmentManager().beginTransaction().add(
-                R.id.container, ForecastFragment.class, null).commit();
-    }
-
-    @Override
-    protected void onStart() {
-        Log.i("USTH Weather", "on start");
-        super.onStart();
-    }
-
-    @Override
-    protected void onRestart() {
-        Log.i("USTH Weather", "on restart");
-        super.onRestart();
-    }
-
-    @Override
-    protected void onResume() {
-        Log.i("USTH Weather", "on resume");
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        Log.i("USTH Weather", "on pause");
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        Log.i("USTH Weather", "on stop");
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        Log.i("USTH Weather", "on destroy");
-        super.onDestroy();
-    }
 }
