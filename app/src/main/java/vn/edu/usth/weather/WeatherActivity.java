5f88cffd09c2af1f3cda78923396022263dd5483
Nguyễn Gia Phong <mcsinyx@disroot.org>
2020-12-16 12:05:32 +0700

    Clean up unused imports

--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,12 +1,6 @@
 package vn.edu.usth.weather;
 
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import vn.edu.usth.weather.ForecastFragment;
 
 public class WeatherActivity extends AppCompatActivity {
     public WeatherActivity() { super(R.layout.activity_weather); }
--- app/src/main/java/vn/edu/usth/weather/WeatherFragment.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherFragment.java
@@ -1,13 +1,5 @@
 package vn.edu.usth.weather;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.LinearLayout;
-
 import androidx.fragment.app.Fragment;
 
 public class WeatherFragment extends Fragment {
