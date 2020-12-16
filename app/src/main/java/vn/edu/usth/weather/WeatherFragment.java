b00fbfa8a39471a24f3c54c117aa64bb33b8fbf1
Nguyễn Gia Phong <mcsinyx@disroot.org>
2020-12-16 12:03:02 +0700

    Add a relative layout for current weather

---
+++ app/src/main/java/vn/edu/usth/weather/WeatherFragment.java
@@ -0,0 +1,15 @@
+package vn.edu.usth.weather;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.LinearLayout;
+
+import androidx.fragment.app.Fragment;
+
+public class WeatherFragment extends Fragment {
+    public WeatherFragment() { super(R.layout.fragment_weather); }
+}
